d015851531651a67fbac075e180c5c98d96433c7
Jesus Almaral <almaral17@gmail.com>
2020-10-06 15:02:50 -0500

    Get user location and show it in map

--- app/src/main/java/com/hackaprende/radardetacos/MapsActivity.kt
+++ app/src/main/java/com/hackaprende/radardetacos/MapsActivity.kt
@@ -1,10 +1,19 @@
 package com.hackaprende.radardetacos
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.location.Location
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import com.google.android.gms.location.LocationServices
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -12,11 +21,14 @@ import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.*
 
+private const val LOCATION_PERMISSION_REQUEST_CODE = 2000
+
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private val taquerias = mutableListOf<Taqueria>()
     private lateinit var tacoIcon: BitmapDescriptor
+    private val userLocation = Location("")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,10 +43,65 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
         tacoIcon = getTacoIcon()
 
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                getUserLocation()
+            } else {
+                requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+            }
+        } else {
+            getUserLocation()
+        }
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun getUserLocation() {
+        val fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
+            if (location != null) {
+                userLocation.latitude = location.latitude
+                userLocation.longitude = location.longitude
+                setupMap()
+            }
+        }
+    }
+
+    private fun setupMap() {
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                getUserLocation()
+            } else if (shouldShowRequestPermissionRationale(Manifest.permission.ACCESS_FINE_LOCATION)) {
+                showLocationPermissionRationaleDialog()
+            } else {
+                finish()
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    private fun showLocationPermissionRationaleDialog() {
+        val dialog = AlertDialog.Builder(this).setTitle("Need location permission")
+                .setMessage("You need to accept this permission to use the app")
+                .setPositiveButton(android.R.string.ok) { _, _ ->
+                    requestPermissions(arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                            LOCATION_PERMISSION_REQUEST_CODE)
+                }.setNegativeButton(android.R.string.no) { _, _ ->
+                    finish()
+                }
+        dialog.show()
     }
 
     /**
@@ -55,7 +122,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             mMap.addMarker(tacoMarker)
         }
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(taquerias[0].latitude, taquerias[0].longitude), 13.0f))
+        val userLatLng = LatLng(userLocation.latitude, userLocation.longitude)
+        val userMarker = MarkerOptions().position(userLatLng)
+        mMap.addMarker(userMarker)
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLatLng, 13.0f))
     }
 
     private fun getTacoIcon(): BitmapDescriptor {

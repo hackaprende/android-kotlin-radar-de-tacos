be76e88f336b64394595698d308a8f2c108bd326
Jesus Almaral <almaral17@gmail.com>
2020-10-06 14:31:15 -0500

    Show fake taquerias in map

--- app/src/main/java/com/hackaprende/radardetacos/MapsActivity.kt
+++ app/src/main/java/com/hackaprende/radardetacos/MapsActivity.kt
@@ -1,22 +1,36 @@
 package com.hackaprende.radardetacos
 
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
+    private val taquerias = mutableListOf<Taqueria>()
+    private lateinit var tacoIcon: BitmapDescriptor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
+
+        taquerias.add(Taqueria("Tacos de Asada", -33.954044, 151.241283))
+        taquerias.add(Taqueria("Tacos de Pastor", -33.967154, 151.264715))
+        taquerias.add(Taqueria("Tacos de Cochinita", -33.9438208,151.2436039))
+        taquerias.add(Taqueria("Tacos de Barbacoa", -33.936577, 151.259410))
+        taquerias.add(Taqueria("Tacos de Birria", -33.9362557,151.2392932))
+        taquerias.add(Taqueria("Burritos", -33.938594, 151.224316))
+
+        tacoIcon = getTacoIcon()
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
@@ -35,9 +49,22 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
-        val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        for (taqueria in taquerias) {
+            val tacoPosition = LatLng(taqueria.latitude, taqueria.longitude)
+            val tacoMarker = MarkerOptions().icon(tacoIcon).position(tacoPosition).title(taqueria.name)
+            mMap.addMarker(tacoMarker)
+        }
+
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(taquerias[0].latitude, taquerias[0].longitude), 13.0f))
+    }
+
+    private fun getTacoIcon(): BitmapDescriptor {
+        val drawable = ContextCompat.getDrawable(this, R.drawable.ic_taco)
+        drawable?.setBounds(0, 0, drawable.intrinsicWidth, drawable.intrinsicHeight)
+        val bitmap = Bitmap.createBitmap(drawable?.intrinsicWidth ?: 0,
+                drawable?.intrinsicHeight ?: 0, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(bitmap)
+        drawable?.draw(canvas)
+        return BitmapDescriptorFactory.fromBitmap(bitmap)
     }
 }
---
+++ app/src/main/java/com/hackaprende/radardetacos/Taqueria.kt
@@ -0,0 +1,3 @@
+package com.hackaprende.radardetacos
+
+data class Taqueria(val name: String, val latitude: Double, val longitude: Double)
